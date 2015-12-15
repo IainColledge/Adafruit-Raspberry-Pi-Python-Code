32a479b89c51c14837434625298fc19c9ed8b0d5
Iain Colledge <iaincolledge@gmail.com>
2015-12-15 22:18:32 +0000

    Example code

--- Adafruit_TSL2561/Adafruit_TSL2561_example.py
+++ Adafruit_TSL2561/Adafruit_TSL2561_example.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
-from Adafruit_TSL2561 import Adafruit_TSL2561
+from Adafruit_TSL2561 import AdafruitTSL2561
 
 # Initialise the sensor
-LightSensor = Adafruit_TSL2561.AdafruitTSL2651()
+LightSensor = AdafruitTSL2561()
 
 # Enable auto gain switching between 1x and 16x
 # Default is False
 LightSensor.enable_auto_gain(True)
 
 # Get the calculated lux value, this is a spot reading so if you're under light
-# lux = Adafruit_TSL2561.calculate_lux()
+lux = LightSensor.calculate_lux()
+
+print ('Lux value is %d',lux)
 
 
